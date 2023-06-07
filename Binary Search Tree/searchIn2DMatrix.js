1877205b56504a80590f13b1cc6c0f5654d36fd1
Danny Wamuya <dwamuya@gmail.com>
2023-06-07 12:09:32 +0300

    search in 2d matrix - optimised

--- Binary Search Tree/searchIn2DMatrix.js
+++ Binary Search Tree/searchIn2DMatrix.js
@@ -38,6 +38,33 @@ var searchMatrix = function (matrix, target) {
   return false;
 };
 
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var search2DMatrix = function (matrix, target) {
+  let rows = matrix.length;
+  let cols = matrix[0].length;
+  let l = 0;
+  let r = rows * cols - 1;
+
+  while (l <= r) {
+    let mid = Math.floor((l + r) / 2);
+    let val = matrix[Math.floor(mid / cols)][mid % cols];
+
+    if (val === target) {
+      return true;
+    } else if (target > val) {
+      l = mid + 1;
+    } else {
+      r = mid - 1;
+    }
+  }
+
+  return false;
+};
+
 console.log(
   searchMatrix(
     [
